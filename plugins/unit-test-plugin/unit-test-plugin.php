7f42518f726f863693465d070407c086dcedd7cc
Matt Ryan <matt.ryan@webdevstudios.com>
2023-06-09 16:35:54 -0400

    Create project

---
+++ plugins/unit-test-plugin/unit-test-plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Unit Test Plugin
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     unit-test-plugin
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Unit_Test_Plugin
+ */
+
+// Your code starts here.
